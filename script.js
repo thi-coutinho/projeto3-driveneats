15d8d9a353b815a320ae289be9169a905e5767e4
thiagocsantos <thi.coutinho.santos@gmail.com>
2022-10-16 21:44:07 -0300

    Envio do pedido - completo

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let prato;
 let bebida;
 let sobremesa;
+let precoPrato;
+let precoBebida;
+let precoSobremesa;
+let mensagem;
 
 function selecionar(botao) {
     // pegar a categoria do pai
@@ -18,26 +22,59 @@ function selecionar(botao) {
     botao.classList.add("selecionado");
     // pegar o nome do item
     let nomeItem = botao.querySelector(".nome-item").innerHTML
+    let precoItemStr = botao.querySelector(".preco").innerHTML
+    let preco = Number(precoItemStr.slice(3).replace(",","."))
     // alterar a opção selecionado na variável prato, bebida ou sobremesa
     if (categoria == "pratos") {
-        prato = nomeItem;
+        prato = nomeItem.trim();
+        precoPrato = preco; 
     } else if (categoria == "bebidas") {
-        bebida = nomeItem;
+        bebida = nomeItem.trim();
+        precoBebida = preco
     } else {
-        sobremesa = nomeItem;
+        sobremesa = nomeItem.trim();
+        precoSobremesa = preco
     }
 
+    // atualizar variáveis
     atualizar();
 
 }
 
 function atualizar() {
+    //atualizar mensagem 
+    mensagem = `Olá, gostaria de fazer o pedido:
+    - Prato: ${prato}
+    - Bebida: ${bebida}
+    - Sobremesa: ${sobremesa}
+    Total: R$ ${(precoSobremesa+precoBebida+precoPrato).toFixed(2)}`
+
     // verificar se os tres itens foram selecionados
     if (prato && bebida && sobremesa) {
         // pegar o botão de finalizar pedido
         const botaoFinalizar = document.querySelector(".base button");
         // adicionar classe que formata o botão de finalizar
         botaoFinalizar.classList.replace("selecionando","finalizado")
+        // mudar o texto do botão
         botaoFinalizar.innerHTML = "Fechar pedido"
+        // mudar o link do botão
+        botaoFinalizar.parentNode.href = `https://wa.me/5511968208712?text=${encodeURIComponent(mensagem)}`
     }
+    
+
 }
+
+// function finalizar(){
+//     if(prato && bebida && sobremesa){
+
+
+//     }
+// }
+
+/* Olá, gostaria de fazer o pedido:
+- Prato: Frango Yin Yang
+- Bebida: Coquinha Gelada
+- Sobremesa: Pudim
+Total: R$ 27.70
+
+*/
